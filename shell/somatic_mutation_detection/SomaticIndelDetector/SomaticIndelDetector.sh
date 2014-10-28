e3a855a5a7b3f837fa01fb212957f06eb18a5c18
Alona A Sosinsky <asoskins@login-3.(none)>
2014-10-28 15:49:05 +0000

    Added SomaticIndelDetector

---
+++ shell/somatic_mutation_detection/SomaticIndelDetector/SomaticIndelDetector.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+## script to run SomaticIndelDetector
+
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:mem=5gb
+
+#PBS -M cgi@imperial.ac.uk
+#PBS -m ea
+#PBS -j oe
+
+#PBS -q pqcgi
+
+# load modules
+module load java/#javaVersion
+module load gatk/2.3-9
+
+NOW="date +%Y-%m-%d%t%T%t"
+JAVA_XMX=4G
+
+# define variables
+ANALYSIS_FILE=#analysisFile
+REFERENCE_FASTA=#referenceFasta
+REFERENCE_DICT=#referenceDict
+NORMAL_BAM=#normalBam
+TUMOR_BAM=#tumorBam
+INTERVALS_FILE=#intervalsFile
+SUMMARY_SCRIPT=#summaryScriptPath
+
+#copy input files to tmp dir
+echo "`${NOW}` copying files to tmp directory..."
+cp $REFERENCE_FASTA $TMPDIR/ref.fasta
+cp $REFERENCE_FASTA.fai $TMPDIR/ref.fasta.fai
+cp $REFERENCE_DICT $TMPDIR/ref.dict
+cp $NORMAL_BAM $TMPDIR/normal.bam
+cp $NORMAL_BAM.bai $TMPDIR/normal.bam.bai
+cp $TUMOR_BAM $TMPDIR/tumor.bam
+cp $TUMOR_BAM.bai $TMPDIR/tumor.bam.bai
+cp $INTERVALS_FILE $TMPDIR/intervals.intervals
+
+# make tmp folder for temporary java files
+mkdir $TMPDIR/tmp
+
+echo "`${NOW}` running  SomaticIndelDetector..."
+java -Xmx$JAVA_XMX -XX:+UseSerialGC -Djava.io.tmpdir=$TMPDIR/tmp -jar $GATK_HOME/GenomeAnalysisTK.jar \
+-T SomaticIndelDetector \
+-I:normal $TMPDIR/normal.bam \
+-I:tumor $TMPDIR/tumor.bam \
+-R $TMPDIR/ref.fasta \
+-L $TMPDIR/intervals.intervals \
+-o $TMPDIR/tmp.vcf \
+-verbose $TMPDIR/tmp.stats
+
+echo "`${NOW}` copying files from tmp directory..."
+cp $TMPDIR/tmp.vcf $ANALYSIS_FILE.vcf
+cp $TMPDIR/tmp.stats $ANALYSIS_FILE.stats
+
+perl $SUMMARY_SCRIPT
---
+++ shell/somatic_mutation_detection/SomaticIndelDetector/merge_chunks.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+## script to merge vcf outputs
+
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:mem=5gb
+
+#PBS -M cgi@imperial.ac.uk
+#PBS -m ea
+#PBS -j oe
+
+#PBS -q pqcgi
+
+NOW="date +%Y-%m-%d%t%T%t"
+
+module load java/#javaVersion
+module load gatk/#gatkVersion
+JAVA_XMX=4G
+NT=2
+
+# define variables
+RESULTS_DIR=#results_dir
+SAMPLE=`basename $RESULTS_DIR`
+RAW_STATS_PATH="#rawStatsFiles"
+IN_VCF="#rawIndelFiles"
+REFERENCE_FASTA=#referenceFasta
+REFRENCE_SEQ_DICT=`echo $REFERENCE_FASTA | perl -pe 's/\.fa/\.dict/'`
+
+#copy reference to $TMP
+cp $REFERENCE_FASTA $TMPDIR/reference.fa
+cp $REFERENCE_FASTA.fai $TMPDIR/reference.fa.fai
+cp $REFRENCE_SEQ_DICT $TMPDIR/reference.dict
+
+# make tmp folder for temporary java files
+mkdir $TMPDIR/tmp
+
+VCF_FILES_COUNT=`echo $IN_VCF | perl -e '$in=<>; @tokens=split(/\s/,$in); $count=@tokens; print $count;'`
+
+# if there is more than one input VCF file
+if [ $VCF_FILES_COUNT -ge 2 ]; then
+
+	# copy VCF files to be merged to temp space and get total number of variants in all vcf files before merging 
+	TMP_IN_VCF=""
+	VARIANT_COUNT_INPUT=0
+       
+	for VCF_FILE in $IN_VCF; do		
+	
+		VCF_BASENAME=`basename $VCF_FILE`
+		grep -P '^#' $VCF_FILE > $TMPDIR/$VCF_BASENAME
+		grep SOMATIC $VCF_FILE >> $TMPDIR/$VCF_BASENAME
+		TMP_IN_VCF="$TMP_IN_VCF -V $TMPDIR/$VCF_BASENAME"
+
+		# get number of variants in the input VCF file
+		VARIANT_COUNT=`grep -v '#' $TMPDIR/$VCF_BASENAME | wc -l`
+		VARIANT_COUNT_INPUT=$(($VARIANT_COUNT_INPUT + $VARIANT_COUNT))
+	done
+
+	java -Xmx$JAVA_XMX -XX:+UseSerialGC -Djava.io.tmpdir=$TMPDIR/tmp -jar $GATK_HOME/GenomeAnalysisTK.jar \
+		-nt $NT \
+		-R $TMPDIR/reference.fa \
+		-T CombineVariants \
+		$TMP_IN_VCF \
+	        --assumeIdenticalSamples \
+		-o $TMPDIR/merged.vcf
+
+	# get number of reads in the output GVCF file
+	VARIANT_COUNT_OUTPUT=`grep -v '#' $TMPDIR/merged.vcf | wc -l`
+	if [[ $VARIANT_COUNT_INPUT -eq $VARIANT_COUNT_OUTPUT ]]; then
+
+		cp $TMPDIR/merged.vcf $RESULTS_DIR/$SAMPLE.vcf
+		chmod 660 $RESULTS_DIR/$SAMPLE.vcf
+
+	else
+
+		echo "Output VCF does not contain the same number of variants as the input VCF files"
+
+        fi
+
+fi
+
+# if there is only one input VCF nothing to merge. Just copy VCF to results directory
+if [ $VCF_FILES_COUNT -eq 1 ]; then
+
+	VCF_BASENAME=`basename $IN_VCF`
+	grep -P '^#' $IN_VCF > $RESULTS_DIR/$SAMPLE.vcf
+	grep SOMATIC $IN_VCF >> $RESULTS_DIR/$SAMPLE.vcf
+	chmod 660 $RESULTS_DIR/$SAMPLE.vcf
+
+fi
+
+RAW_STATS_MERGED=$RESULTS_DIR/$SAMPLE.stats
+cat $RAW_STATS_PATH > $RAW_STATS_MERGED
+chmod 660 $RAW_STATS_MERGED
+
+grep SOMATIC $RAW_STATS_MERGED > $RAW_STATS_MERGED.somatic
+chmod 660 $RAW_STATS_MERGED.somatic
+
+ls -l
+
+
+
+
+
