65cb7947791a722938b021b92adc6f557fc50f38
Alona A Sosinsky <asoskins@login-3.(none)>
2014-12-12 14:58:21 +0000

    Oncotator: added oncotator scripts

---
+++ shell/somatic_mutation_detection/oncotator/merge_vcf.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+## script to run merge vcf files for the same sample
+
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:mem=5gb
+
+#PBS -M cgi@imperial.ac.uk
+#PBS -m ea
+#PBS -j oe
+
+#PBS -q pqcgi
+
+module load oncotator/#oncotatorVersion
+module load python/#pythonVersion
+module load samtools/#samtoolsVersion
+module load tabix/#tabixVersion
+
+module load java/#javaVersion
+module load gatk/#gatkVersion
+JAVA_XMX=4G
+NT=2
+
+INPUT_VCF_MUTECT=#pathVCFmutect
+INPUT_VCF_SID=#pathVCFsid
+ANALYSIS_DIR=#analysisDir
+SAMPLE_NAME=`basename $INPUT_VCF_SID .vcf`
+REFERENCE_FASTA=#referenceFasta
+REFRENCE_SEQ_DICT=`echo $REFERENCE_FASTA | perl -pe 's/\.fa/\.dict/'`
+
+#copy reference to $TMP
+cp $REFERENCE_FASTA $TMPDIR/reference.fa
+cp $REFERENCE_FASTA.fai $TMPDIR/reference.fa.fai
+cp $REFRENCE_SEQ_DICT $TMPDIR/reference.dict
+
+mkdir $TMPDIR/oncotator_db
+cp -R /groupvol/cgi/resources/annotations/oncotator_v1_ds_Sept172014/* $TMPDIR/oncotator_db
+ls -l $TMPDIR/oncotator_db
+
+#copy vcf
+cp $INPUT_VCF_MUTECT $TMPDIR/mutect.vcf
+cp $INPUT_VCF_SID $TMPDIR/SID.vcf
+
+# make tmp folder for temporary java files
+mkdir $TMPDIR/tmp
+
+# get number of variants in the input VCF file
+VARIANT_COUNT_MUTECT=`grep -v '#' $TMPDIR/mutect.vcf | wc -l`
+VARIANT_COUNT_SID=`grep -v '#' $TMPDIR/SID.vcf | wc -l`
+VARIANT_COUNT_INPUT=$(($VARIANT_COUNT_MUTECT + $VARIANT_COUNT_SID))
+
+echo "`$NOW`merging Mutect and SomaticIndelDetector VCF files"
+java -Xmx$JAVA_XMX -XX:+UseSerialGC -Djava.io.tmpdir=$TMPDIR/tmp -jar $GATK_HOME/GenomeAnalysisTK.jar \
+        -nt $NT \
+        -R $TMPDIR/reference.fa \
+        -T CombineVariants \
+        -V $TMPDIR/mutect.vcf \
+        -V $TMPDIR/SID.vcf \
+	--assumeIdenticalSamples \
+	-o $TMPDIR/merged.vcf
+
+echo "`$NOW` run Oncotator on VCF files"
+oncotator -i VCF --db-dir $TMPDIR/oncotator_db -o VCF $TMPDIR/merged.vcf $TMPDIR/annotated.vcf hg19
+
+echo "`$NOW` copy VCF files"
+# get number of reads in the output VCF file
+VARIANT_COUNT_OUTPUT=`grep -v '#' $TMPDIR/annotated.vcf | wc -l`
+
+if [[ $VARIANT_COUNT_INPUT -le $VARIANT_COUNT_OUTPUT ]]; then
+
+        cp $TMPDIR/merged.vcf $ANALYSIS_DIR/${SAMPLE_NAME}.vcf
+	chmod 660 $ANALYSIS_DIR/${SAMPLE_NAME}.vcf
+
+	cat $TMPDIR/annotated.vcf |perl -e 'while (<>) { s/(^\S+\t\S+\t\S+\t\S+\t\S+\t\S+\t\S+\t)\"(\S+)\"(\t.*)/$1$2$3/g; s/^\<(\S+)\>(.*)/$1$2/; print; }' > $ANALYSIS_DIR/${SAMPLE_NAME}.annotated.vcf
+	chmod 660 $ANALYSIS_DIR/${SAMPLE_NAME}.annotated.vcf
+
+else
+
+	echo "Output VCF does not contain the same number of variants as the input VCF files"
+	exit 1
+
+fi
---
+++ shell/somatic_mutation_detection/oncotator/oncotator.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+## script to merge vcf files across all samples and to run Oncotator on the merged file
+
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:mem=10gb
+
+#PBS -M cgi@imperial.ac.uk
+#PBS -m ea
+#PBS -j oe
+
+#PBS -q pqcgi
+
+module load oncotator/#oncotatorVersion
+module load python/#pythonVersion
+module load samtools/#samtoolsVersion
+module load tabix/#tabixVersion
+
+module load gatk/#gatkVersion
+module load java/#javaVersion
+JAVA_XMX=4G
+NT=2
+
+IN_VCF="#multisampleVCF"
+PROJECT=#project
+RESULTS_DIR=#resultsDir
+SAMPLE_NAME=`basename $INPUT_VCF_SID .vcf`
+REFERENCE_FASTA=#referenceFasta
+REFRENCE_SEQ_DICT=`echo $REFERENCE_FASTA | perl -pe 's/\.fa/\.dict/'`
+
+#copy reference to $TMP
+cp $REFERENCE_FASTA $TMPDIR/reference.fa
+cp $REFERENCE_FASTA.fai $TMPDIR/reference.fa.fai
+cp $REFRENCE_SEQ_DICT $TMPDIR/reference.dict
+
+mkdir $TMPDIR/oncotator_db
+cp -R /groupvol/cgi/resources/annotations/oncotator_v1_ds_Sept172014/* $TMPDIR/oncotator_db
+ls -l $TMPDIR/oncotator_db
+
+# make tmp folder for temporary java files
+mkdir $TMPDIR/tmp
+
+#merge vcf files across all samples
+echo "`$NOW` mergeing VCF files across all samples"
+
+VCF_FILES_COUNT=`echo $IN_VCF | perl -e '$in=<>; @tokens=split(/\s/,$in); $count=@tokens; print $count;'`
+
+# if there is more than one input VCF file
+if [ $VCF_FILES_COUNT -ge 2 ]; then
+
+	# copy VCF files to be merged to temp space 
+	TMP_IN_VCF=""
+       
+	for VCF_FILE in $IN_VCF; do		
+	
+		VCF_BASENAME=`basename $VCF_FILE`
+		cp $VCF_FILE $TMPDIR/$VCF_BASENAME
+		TMP_IN_VCF="$TMP_IN_VCF -V $TMPDIR/$VCF_BASENAME"
+
+	done
+
+	java -Xmx$JAVA_XMX -XX:+UseSerialGC -Djava.io.tmpdir=$TMPDIR/tmp -jar $GATK_HOME/GenomeAnalysisTK.jar \
+		-nt $NT \
+		-R $TMPDIR/reference.fa \
+		-T CombineVariants \
+		$TMP_IN_VCF \
+		-o $TMPDIR/merged.vcf
+
+elif [ $VCF_FILES_COUNT -eq 1 ]; then
+
+	cp $IN_VCF $TMPDIR/merged.vcf
+
+fi
+
+cp $TMPDIR/merged.vcf $RESULTS_DIR/${PROJECT}.vcf
+chmod 660 $RESULTS_DIR/${PROJECT}.vcf
+
+echo "`$NOW` run Oncotator on VCF file"
+oncotator -i VCF --db-dir $TMPDIR/oncotator_db -o VCF $TMPDIR/merged.vcf $TMPDIR/annotated.vcf hg19
+cat $TMPDIR/annotated.vcf |perl -e 'while (<>) { s/(^\S+\t\S+\t\S+\t\S+\t\S+\t\S+\t\S+\t)\"(\S+)\"(\t.*)/$1$2$3/g; s/^\<(\S+)\>(.*)/$1$2/; print; }' > $RESULTS_DIR/${PROJECT}.annotated.vcf
+chmod 660 $RESULTS_DIR/${PROJECT}.annotated.vcf
+
+oncotator -i VCF --db-dir $TMPDIR/oncotator_db -o TCGAMAF --skip-no-alt $TMPDIR/merged.vcf $TMPDIR/annotated.maf hg19
+cp $TMPDIR/annotated.maf $RESULTS_DIR/${PROJECT}.annotated.maf
+chmod 660 $RESULTS_DIR/${PROJECT}.annotated.maf
+
