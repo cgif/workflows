5085727b92c0a001f9631ceefb452acbec0132cb
Dalia Kasperaviciute <d.kasperaviciute@imperial.ac.uk>
2015-01-21 16:52:26 +0000

    Added small_rna workflow   -added edgeR workflow for paired samples to small_rna workflow

---
+++ shell/small_rna/edgeR/edgeR_paired_DE.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# runs edgeR on paired samples
+#
+
+#PBS -l walltime=#walltimeHours:00:00
+#PBS -l ncpus=1
+#PBS -l mem=2gb
+
+#PBS -M cgi@imperial.ac.uk
+#PBS -m bea
+#PBS -j oe
+
+module load R/3.1.0
+
+RESULT_DIR=#resultsDir
+R_SCRIPT=#rScript 
+
+R CMD BATCH --no-save --no-restore $R_SCRIPT ${R_SCRIPT}.log
+
+chmod 660 $RESULT_DIR/*
