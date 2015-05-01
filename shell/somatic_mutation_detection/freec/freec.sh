cbec329978d86c0b36b61a4d4b911ee0f4b649e6
Alona A Sosinsky <asoskins@login-2.(none)>
2015-05-01 16:37:13 +0100

    Optimized running for FREEC

--- shell/somatic_mutation_detection/freec/freec.sh
+++ shell/somatic_mutation_detection/freec/freec.sh
@@ -15,9 +15,11 @@ module load freec/#freecVersion
 module load samtools/#SamtoolsVersion
 module load R/#Rversion
 
+SEX=#Sex
 CHROM_LENGTH=#ChrLenFile
-FREEC_PROFILE=#freecProfile
 SAMTOOLS_VERSION=#SamtoolsVersion
+
+FREEC_PROFILE=#freecProfile
 FREEC_GRAPH=#freecGraph
 FREEC_SIGN=#freecSign
 
@@ -25,13 +27,23 @@ RESULTS_DIR=#ResultsDir
 NORMAL_BAM=#NormalBam
 TUMOR_BAM=#TumorBam
 
+WINDOW=#Window
+STEP=#Step
+PLOIDY=#Ploidy
+BREAK_POINT=#BreakPoint
+
+REF_FASTA=#RefFasta
+TMP_REF_FASTA=$TMPDIR/ref.fasta
+cp $REF_FASTA $TMP_REF_FASTA
+cp $REF_FASTA.fai $TMP_REF_FASTA.fai
+
 TMP_NORMAL_BAM=$TMPDIR/normal.bam
 cp $NORMAL_BAM $TMP_NORMAL_BAM
 
 #filter reads with non-uniq mapping
 samtools view -h $TMPDIR/normal.bam|awk '!($5 == 0) {print}'|samtools view -hSb -  > $TMPDIR/normal.filtered.bam
 ls -lh
-mv $TMPDIR/normal.filtered.bam $TMPDIR/normal.bam
+mv $TMPDIR/normal.filtered.bam $TMPDIR/normal.bam 
 samtools index $TMPDIR/normal.bam
 
 TMP_TUMOR_BAM=$TMPDIR/tumor.bam
@@ -52,9 +64,16 @@ cp $FREEC_PROFILE $TMP_FREEC_PROFILE
 sed -i -e "s/#resultsDir/${TMPDIR//\//\\/}/" $TMP_FREEC_PROFILE
 sed -i -e "s/#normalBam/${TMP_NORMAL_BAM//\//\\/}/" $TMP_FREEC_PROFILE
 sed -i -e "s/#tumorBam/${TMP_TUMOR_BAM//\//\\/}/" $TMP_FREEC_PROFILE
-
+sed -i -e "s/#sex/${SEX}/" $TMP_FREEC_PROFILE
 sed -i -e "s/#chrLenFile/${TMP_CHROM_LENGTH//\//\\/}/" $TMP_FREEC_PROFILE
 sed -i -e "s/#samtoolsVersion/${SAMTOOLS_VERSION//\//\\/}/" $TMP_FREEC_PROFILE
+sed -i -e "s/#window/${WINDOW}/" $TMP_FREEC_PROFILE
+sed -i -e "s/#step/${STEP}/" $TMP_FREEC_PROFILE
+sed -i -e "s/#polidy/${PLOIDY}/" $TMP_FREEC_PROFILE
+sed -i -e "s/#breakPoint/${BREAK_POINT}/" $TMP_FREEC_PROFILE
+
+ls -l 
+more $TMP_FREEC_PROFILE
 
 freec -conf $TMP_FREEC_PROFILE
 
