4b60cf764f3de26799bc58ef91d4d81fa94bb332
Alona A Sosinsky <asoskins@login-2.(none)>
2015-10-15 15:05:33 +0100

    FIxed input options for bam file pre-processing in differential methylation workflow

--- shell/methylation_analysis/filter/filterandrename.sh
+++ shell/methylation_analysis/filter/filterandrename.sh
@@ -15,21 +15,21 @@ module load samtools/#samtoolsVersion
 MERGETAG_DIR="#mergetagDir"
 FILTERED_DIR="#filteredDir"
 SAMPLE="#sample"
+RENAME_CHROM="#renameChrom"
 
 cp $MERGETAG_DIR/$SAMPLE/$SAMPLE.bam $TMPDIR/$SAMPLE.bam
 
 echo "removing duplicates and unmapped reads and renaming chromosome names to hg19 from sample $SAMPLE..."
 
 samtools view -hF 1028 $TMPDIR/$SAMPLE.bam \
-| perl -e 'open(CHROM,"/groupvol/cgi/resources/reference/eukaryote/human/hs37d5_to_hg19.chrom_names.txt"); %rename = (); while (<CHROM>){ /(.*)\t(.*)/; $rename{$1} = $2;} while(<>) {if (/^@/ && /SN:/) { s/(.*\tSN:)(.*)(\t.*)/$1$rename{$2}$3/; print } elsif (/^@/) { print } else { @cols = split(/\t/); $cols[2] =~ s/(.*)/$rename{$1}/; $cols[6] =~ s/(.*)/$rename{$1}/ unless $cols[6] eq "="; $line = ""; foreach $col (@cols) { $line .= $col."\t"; } chop($line); print "$line" } }' \
-| samtools view -bS - > $TMPDIR/$SAMPLE.nondup.rename.filt.bam
+| perl -e '$rename_chrom = shift; open(CHROM,"$rename_chrom"); %rename = (); while (<CHROM>){ /(.*)\t(.*)/; $rename{$1} = $2;} while(<>) {if (/^@/ && /SN:/) { s/(.*\tSN:)(.*)(\t.*)/$1$rename{$2}$3/; print } elsif (/^@/) { print } else { @cols = split(/\t/); $cols[2] =~ s/(.*)/$rename{$1}/; $cols[6] =~ s/(.*)/$rename{$1}/ unless $cols[6] eq "="; $line = ""; foreach $col (@cols) { $line .= $col."\t"; } chop($line); print "$line" } }' $RENAME_CHROM | samtools view -bS - > $TMPDIR/$SAMPLE.nondup.rename.filt.bam
 
 samtools index $TMPDIR/$SAMPLE.nondup.rename.filt.bam
 
 cp $TMPDIR/$SAMPLE.nondup.rename.filt.bam $FILTERED_DIR/$SAMPLE/$SAMPLE.nondup.rename.filt.bam
 cp $TMPDIR/$SAMPLE.nondup.rename.filt.bam.bai $FILTERED_DIR/$SAMPLE/$SAMPLE.nondup.rename.filt.bam.bai
 
-
+ls -lh
 
 
 
