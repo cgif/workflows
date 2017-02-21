2812c23f6a5ae40d81d0a1635565c188cdc1fb83
Dalia Kasperaviciute <d.kasperaviciute@imperial.ac.uk>
2017-02-21 12:06:28 +0000

    HTSeq_summary.pl corrected to calculate percentages using resd pairs qcontamination changed to allow longer walltime for imsa

--- shell/rna_seq/htseq/HTSeq_summary.pl
+++ shell/rna_seq/htseq/HTSeq_summary.pl
@@ -25,15 +25,15 @@ while (defined(my $sample = readdir(PROJECT))){
     next if $sample =~ /^\./;
     next if $sample eq "multisample";
 
-    my $flagstat = "$input_dir/$sample/$sample.bam.flagstat";
+    my $flagstat = "$input_dir/$sample/$sample.alignment_summary_metrics";
     if (-f "$flagstat"){
-        $sum{$sample}{'total'} = `grep total $flagstat`;
-        $sum{$sample}{'total'} =~ s/^(\d+)\s.*/$1/;
+        $sum{$sample}{'total'} = `grep FIRST_OF_PAIR $flagstat`;	 
+        $sum{$sample}{'total'} =~ s/\w+\s+(\d+)\s.*/$1/;
     }
 
     my $table = "$project_dir_results/$sample/$sample.HTSeq.counts.discard";
     next unless (-f "$table");
-    foreach my $param qw(no_feature ambiguous too_low_aQual not_aligned alignment_not_unique){
+    foreach my $param (qw(no_feature ambiguous too_low_aQual not_aligned alignment_not_unique)){
         $sum{$sample}{$param} = `tail $table|grep '$param'`;
         $sum{$sample}{$param} =~ s/\w+\s+(\d+)/$1/;
     }
@@ -45,21 +45,21 @@ open (OUT, ">$summary_results/index.html");
 print OUT "<HTML>";
 print OUT "<TABLE CELLPADDING=5><TR>";
 print OUT "<TH ROWSPAN = 2><CENTER>Sample";
-print OUT "<TH ROWSPAN = 2><CENTER>Total reads";
-print OUT "<TH COLSPAN = 5><CENTER>Discarded reads";
-print OUT "<TR><TH><CENTER>Reads that don't overlap with any features";
-print OUT "<TH><CENTER>Reads that overlap with > 1 feature";
-print OUT "<TH><CENTER>Reads with alignment quality below 10";
-print OUT "<TH><CENTER>Reads that did not align";
-print OUT "<TH><CENTER>Reads that align to multiple locations";
+print OUT "<TH ROWSPAN = 2><CENTER>Total read pairs";
+print OUT "<TH COLSPAN = 5><CENTER>Discarded read pairs";
+print OUT "<TR><TH><CENTER>Read pairs that don't overlap with any features";
+print OUT "<TH><CENTER>Read pairs that overlap with > 1 feature";
+print OUT "<TH><CENTER>Read pairs with alignment quality below 10";
+print OUT "<TH><CENTER>Read pairs that did not align";
+print OUT "<TH><CENTER>Read pairs that align to multiple locations";
 print OUT "\n";
 
-foreach my $sample (keys %sum){
+foreach my $sample (sort keys %sum){
 
     print OUT "<TR><TD>$sample"; 
     print OUT "<TD><CENTER>$sum{$sample}{'total'}"; 
 
-    foreach my $param qw(no_feature ambiguous too_low_aQual not_aligned alignment_not_unique){
+    foreach my $param (qw(no_feature ambiguous too_low_aQual not_aligned alignment_not_unique)){
         print OUT "<TD><CENTER>$sum{$sample}{$param}";
 	if (defined($sum{$sample}{'total'})){
 	    my $percent = ($sum{$sample}{$param}/$sum{$sample}{'total'})*100;
